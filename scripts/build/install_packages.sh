58851bd4204e3556ada804afafede043cffcf1ee
Koo <koo@Kooui-MacBookAir.local>
2024-09-27 12:14:27 +0900

    MacOS에서 Makefile 작동하도록 변경함

---
+++ scripts/build/install_packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source scripts/settings.sh
+
+info Installing packages...
+python -m pip install -r requirements/requirements.txt
+
+if [[ "${INSTALL_TEST_PACKAGES}" == "yes" ]]; then
+    info Installing test packages...
+    python -m pip install -r requirements/test_requirements.txt
+fi
--- scripts/settings.sh
+++ scripts/settings.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-# Python
-PYTHON_VERSION=3.12
-PYTHON_BIN=/usr/local/bin/python${PYTHON_VERSION}
-
-# virtualenv
-VIRTUALENV_BIN="${HOME}/.local/bin/virtualenv"  # ex) ${HOME}/.local/bin/virtualenv
-VIRTUALENV_DIR=.venv
-VIRTUALENV_ACTIVATE=${VIRTUALENV_DIR}/bin/activate
-
 FG_RESET='\033[0m'
 FG_BLACK='\033[0;30m'
 FG_RED='\033[0;31m'
@@ -33,8 +23,37 @@ function error() {
     echo -e "${FG_RED}[E] $@${FG_RESET}" 1>&2
 }
 
+# Python
+PYTHON_VERSION=3.12
+PYTHON_BIN=/usr/local/bin/python${PYTHON_VERSION}
+
 # Check requirements
 if [[ "`which ${PYTHON_BIN}`" == "" ]]; then
     echo "Python ${PYTHON_VERSION} is not installed. Please install it."
+    exit 1
+fi
+
+VIRTUALENV_DIR=.venv
+VIRTUALENV_ACTIVATE=${VIRTUALENV_DIR}/bin/activate
+
+OS_PLATFORM="`uname -s`"
+case "${OS_PLATFORM}" in
+    "Darwin")
+        # ex) /System/Volumes/Data/Library/Frameworks/Python.framework/Versions/3.12/bin/virtualenv
+        VIRTUALENV_BIN="/System/Volumes/Data/Library/Frameworks/Python.framework/Versions/${PYTHON_VERSION}/bin/virtualenv"
+        ;;
+    "Linux")
+        # ex) ${HOME}/.local/bin/virtualenv
+        VIRTUALENV_BIN="${HOME}/.local/bin/virtualenv"
+        ;;
+    *)
+        echo "Unsupported OS: ${OS_PLATFORM}"
+        exit 1
+        ;;
+esac
+
+# virtualenv
+if [[ ! -f "${VIRTUALENV_BIN}" ]]; then
+    error "virtualenv is not installed. Please install it.\n\t ${PYTHON_BIN} -m pip install virtualenv"
     exit 1
 fi
